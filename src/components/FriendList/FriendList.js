d05a9d4d7986c07d95c9188d778d1eb07187ed2e
Demian-Pashkovskyi <skyangely4@gmail.com>
2022-08-06 23:40:08 +0300

    friend v-2.0

---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import st from './FriendList.module.css';
+// import { FriendListItem } from '../FriendList/FriendListItem';
+import { FriendListItem } from './FriendListItem';
+
+export const FriendList = ({ friends }) => {
+  return (
+		<ul className={st.list}>
+			<FriendListItem friends={friends} />
+		</ul>
+	)
+}
+
+FriendListItem.propTypes = {
+	friends: PropTypes.array.isRequired,
+};
